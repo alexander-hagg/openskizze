14735f06e09f39b4a5e0451b85becace8c890c27
Alexander Hagg <alexander.hagg@h-brs.de>
2024-10-30 17:05:51 +0100

    Moved dash code to root

--- src/mapelites/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Import relevant parts for MAP-Elites."""
-from . import archive, evolution, visualize_phenotypes
-
-__all__ = ["archive", "evolution", "visualize_phenotypes"]
--- test/test_gdal.py
+++
@@ -1,6 +0,0 @@
-# import gdal
-from osgeo import gdal
-dsm_path = 'data/Goteborg_SWEREF99_1200/DSM_KRbig.tif'
-ds = gdal.Open(dsm_path)
-dsm_data = ds.GetRasterBand(1).ReadAsArray()
-print(dsm_data)
